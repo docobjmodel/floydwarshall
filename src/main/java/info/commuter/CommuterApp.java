96ba6f65f213f695f65aefd4c36cd0ea39c3e862
boris <docobjmodel@excite.com>
2016-01-29 11:18:21 +0100

    presented two methods for output results

--- src/main/java/info/commuter/CommuterApp.java
+++ src/main/java/info/commuter/CommuterApp.java
@@ -3,6 +3,7 @@
  */
 package info.commuter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,19 +27,30 @@ public class CommuterApp {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 		ctx.register(CommuterFileConfig.class);
 		ctx.refresh();
-		// Floyd-Warshall algorithm here
-		Map<Integer, List<Integer>> paths = (new FloydWarschall())
-				.getShortestPaths(new FileActor((AddressBean) ctx.getBean("inputAddress")).getGraph());
 		String space = " ";
 		String gap = " : ";
 		Integer integerMaxValue = Integer.valueOf(Integer.MAX_VALUE);
-		System.out.println("-------- shortest paths presentation --------");
+		// Floyd-Warshall algorithm here
+		Map<Integer, List<Integer>> paths = (new FloydWarschall())
+				.getShortestPaths(new FileActor((AddressBean) ctx.getBean("inputAddress")).getGraph());
+		System.out.println("-------- implicit Java 8 traversal : shortest paths presentation ---------");
 		paths.keySet().forEach(station -> {
 			System.out.print(station.toString().concat(gap));
 			paths.get(station).forEach(
 					route -> System.out.print((route.equals(integerMaxValue) ? "-" : route).toString().concat(space)));
 			System.out.println();
 		});
+		System.out.println("-------------------------------------------------------------------------");
+		System.out.println("----------- explicit traversal : shortest paths presentation ------------");
+		List<Integer> stations = new ArrayList<Integer>();
+		paths.keySet().forEach(stations::add);
+		stations.sort((st1, st2) -> st1.compareTo(st2));
+		for (Integer station : stations) {
+			System.out.print(station.toString().concat(gap));
+			paths.get(station).forEach(route -> System.out.print(route.toString().concat(space)));
+			System.out.println();
+		}
+		System.out.println("-------------------------------------------------------------------------");
 		//
 		ctx.close();
 	}
